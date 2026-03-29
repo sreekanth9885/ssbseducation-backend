1c543e202fab4452ccd11fd73cc53590c166fd96
Srikanth Bethi <srikanth.bethi01@gmail.com>
2026-03-29 14:31:48 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Credentials: true");
+
+// Handle preflight request
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 header("Content-Type: application/json");
 
 require_once "config/Database.php";
